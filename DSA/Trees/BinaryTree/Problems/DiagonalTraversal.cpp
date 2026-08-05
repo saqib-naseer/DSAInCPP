4bed6ad7242a1cd4bbb35c5c98cbddc2a0d68f66
saqib-naseer <saaqibnaseer@gmail.com>
2026-08-05 13:05:49 +0000

    Diagonal Traversal of BT

---
+++ DSA/Trees/BinaryTree/Problems/DiagonalTraversal.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+  Node(int data) {
+    this->data = data;
+    this->left = this->right = NULL;
+  }
+};
+
+void LevelOrder(Node *root) {
+  if (root == NULL)
+    return;
+
+  queue<Node *> q;
+  q.push(root);
+
+  while (!q.empty()) {
+    Node *temp = q.front();
+    q.pop();
+    cout << temp->data << " ";
+    if (temp->left)
+      q.push(temp->left);
+    if (temp->right)
+      q.push(temp->right);
+  }
+}
+
+
+void FindL(Node* root,int pos,int&l){
+
+	if(!root) return;
+	l = max(pos,l);
+	FindL(root->left,pos+1,l);
+	FindL(root->right,pos,l);
+
+}
+
+void Traverse(Node* root,int left, vector<vector<int>>& ans){
+
+	if(!root) return;
+  
+	ans[left].push_back(root->data);
+  
+	Traverse(root->left,left+1,ans);
+	Traverse(root->right,left,ans);
+
+}
+
+void DiagonalTraverse(Node* root){
+
+	if(!root) return;
+	int l=0;
+	FindL(root,0,l);
+	vector<vector<int>> ans(l+1);
+  cout<<"L value is "<<l<<endl;
+	Traverse(root,0,ans);
+	
+	//Iterate Ans Array
+	
+	for(int i=0; i<ans.size();i++){
+	for(int j=0;j<ans[i].size();j++){
+	cout<<ans[i][j]<< " " ;
+	}
+	
+	}
+
+}
+
+
+
+
+int main() {
+  Node *root = new Node(1);
+  Node *second = root->left = new Node(2);
+  Node *third = root->right = new Node(3);
+
+  Node *four = second->left = new Node(4);
+  Node *five = second->right = new Node(5);
+
+  Node *six = third->left = new Node(6);
+  Node *seven = third->right = new Node(7);
+
+  Node *eight = four->left = new Node(8);
+  Node *nine = four->right = new Node(9);
+
+  Node *ten = five->left = new Node(10);
+  Node *eleven = five->right = new Node(11);
+
+  Node *twelve = six->left = new Node(12);
+  Node *thirteen = six->right = new Node(13);
+
+  Node *fourteen = seven->left = new Node(14);
+  Node *fifteen = seven->right = new Node(15);
+
+  cout << "LevelOrder: " << endl;
+  LevelOrder(root);
+
+  cout << "\nDiagonal Traversal: " << endl;
+  DiagonalTraverse(root);
+  
+}
